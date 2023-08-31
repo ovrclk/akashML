6ffad9cef34b404af38711217654b1e67e312e78
Andrey Arapov <andrey@akash.network>
2023-08-31 15:07:13 +0200

    flatten base url as akashml.com points to gh-pages now
    npm run build
    npm run deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/akashML/',  // Make sure this matches your GitHub Pages subdirectory
+  base: '/',  // Make sure this matches your GitHub Pages subdirectory if used
   plugins: [react()],
 })
